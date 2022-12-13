dafd5449c995a3ab9b3b19607720d3f86238defe
aldy15 <102945571+aldy15@users.noreply.github.com>
2022-12-13 21:14:11 +0700

    pembuatan selesai

---
+++ assets/js/variables.js
@@ -0,0 +1,132 @@
+const variables = {
+  greetings: {
+    morning: "Selamat Ulang Tahun",
+    day: "Selamat Ulang Tahun",
+    afternoon: "Selamat Ulang Tahun",
+    evening: "Selamat Ulang Tahun",
+    night: "Selamat Ulang Tahun",
+  },
+  quotes: [
+    {
+      quote: '" Panjang umur dan Sehat Selalu, Kami sayang Papa dan Mama :) "',
+      author: "Anak-anak Papa dan Mama ~",
+    },
+    //{ quote: "I love you.", author: "Brian" },
+    //{ quote: "Aku sayang kamu!", author: "Ian" },
+    //{ quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
+    //{ quote: "Love chuuu", author: "Bribri" },
+  ],
+  nicknames: ["Papa dan Mama"],
+  generated: ["{greetings}, {nicknames} :)"],
+};
+
+const particlesConfig = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 8000,
+      },
+    },
+    color: {
+      value: "#000",
+    },
+    shape: {
+      type: "image",
+      stroke: {
+        width: 2,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "./assets/img/heart2.png",
+        width: 60,
+        height: 60,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 14,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 2,
+        size_min: 0.5,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 80,
+      color: "#000",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "right",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 150,
+        line_linked: {
+          opacity: 0.3,
+        },
+      },
+      bubble: {
+        distance: 800,
+        size: 80,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 2,
+      },
+      remove: {
+        particles_nb: 1,
+      },
+    },
+  },
+  retina_detect: true,
+};
